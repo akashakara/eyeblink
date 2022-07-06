0c6aaeca78f8791871a1501de678ade70fa046c5
akashxxvi <akash.akara@gmail.com>
2022-07-06 16:31:32 +0530

    final

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 
 EYE_AR_THRESH = 0.23
 EYE_AR_CONSEC_FRAMES = 3
-
+EYE_AR_CONSEC_FRAMES2=6
+EYE_AR_CONSEC_FRAMES3=10
 eye_landmarks = "model_landmarks/shape_predictor_68_face_landmarks.dat"
 
 COUNTER = 0
